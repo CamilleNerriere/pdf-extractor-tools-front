b2a9b52c8b1ae81a554a46b12198d456731ba691
CamilleNerriere <camille.nerriere@yahoo.fr>
2025-06-07 09:48:05 +0200

    create header and footer components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports:[RouterOutlet, Header, Footer]
 })
 export class App {
   protected title = 'pdf-tools-front';
---
+++ src/app/extractor/extractor.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-extractor',
+  imports: [],
+  templateUrl: './extractor.html',
+  styleUrl: './extractor.scss'
+})
+export class Extractor {
+
+}
